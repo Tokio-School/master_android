3f3b62d9d94ddc03598ff87a65959004357b0106
manelcc <manelcc72@gmail.com>
2021-08-08 09:34:36 +0200

    TP6_CONSTRAINT LAYOUT: add project constraint

---
+++ TP6_CONSTRAINT_LAYOUT/app/src/main/java/com/tokioschol/tp6_constraint_layout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tokioschol.tp6_constraint_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
