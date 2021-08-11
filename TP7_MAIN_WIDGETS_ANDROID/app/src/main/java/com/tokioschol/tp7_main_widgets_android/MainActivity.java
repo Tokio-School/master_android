11ea0104d430b15a6020afb3b5735f31728c3f3b
manelcc <manelcc72@gmail.com>
2021-08-11 19:23:48 +0200

    agregar spinner dinamico y spinner estatico

--- TP7_MAIN_WIDGETS_ANDROID/app/src/main/java/com/tokioschol/tp7_main_widgets_android/MainActivity.java
+++ TP7_MAIN_WIDGETS_ANDROID/app/src/main/java/com/tokioschol/tp7_main_widgets_android/MainActivity.java
@@ -2,13 +2,27 @@ package com.tokioschol.tp7_main_widgets_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.tokioschol.tp7_main_widgets_android.databinding.ActivityMainBinding;
+import com.tokioschol.tp7_main_widgets_android.widgets.CardViewActivity;
+import com.tokioschol.tp7_main_widgets_android.widgets.SpinnerActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.activitySpinner.setOnClickListener(view -> startActivity(new Intent(this, SpinnerActivity.class)));
+        binding.activityCardView.setOnClickListener(view -> startActivity(new Intent(this, CardViewActivity.class)));
+
     }
 }
---
+++ TP7_MAIN_WIDGETS_ANDROID/app/src/main/java/com/tokioschol/tp7_main_widgets_android/widgets/CardViewActivity.java
@@ -0,0 +1,16 @@
+package com.tokioschol.tp7_main_widgets_android.widgets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tokioschol.tp7_main_widgets_android.R;
+
+public class CardViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_view);
+    }
+}
