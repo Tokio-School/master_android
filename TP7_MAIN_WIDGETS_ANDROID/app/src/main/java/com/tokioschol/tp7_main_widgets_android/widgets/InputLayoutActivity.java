f13c46e6fd1fd25717571e6cb02f0d6ae39b92c2
manelcc <manelcc72@gmail.com>
2021-08-24 12:11:25 +0200

    TP7 WIDGETS: cardview

---
+++ TP7_MAIN_WIDGETS_ANDROID/app/src/main/java/com/tokioschol/tp7_main_widgets_android/widgets/InputLayoutActivity.java
@@ -0,0 +1,16 @@
+package com.tokioschol.tp7_main_widgets_android.widgets;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tokioschol.tp7_main_widgets_android.R;
+
+public class InputLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_view);
+    }
+}
