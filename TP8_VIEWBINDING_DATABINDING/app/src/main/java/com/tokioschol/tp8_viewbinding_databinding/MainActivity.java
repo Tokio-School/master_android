0786ff91f4976b4b48c96f750b6deef98c0dca18
manelcc <manelcc72@gmail.com>
2021-08-31 20:34:29 +0200

    TP8 VIEWBINDING - DATABINDING: viewbinding activity and fragment

--- TP8_VIEWBINDING_DATABINDING/app/src/main/java/com/tokioschol/tp8_viewbinding_databinding/MainActivity.java
+++ TP8_VIEWBINDING_DATABINDING/app/src/main/java/com/tokioschol/tp8_viewbinding_databinding/MainActivity.java
@@ -3,12 +3,28 @@ package com.tokioschol.tp8_viewbinding_databinding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.tokioschol.tp8_viewbinding_databinding.databinding.ActivityMainBinding;
+import com.tokioschol.tp8_viewbinding_databinding.viewbinding.ViewBindingInflateFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //2 Debemos recuperar la vista a utilizar
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //3 Ahora ya podemos utilizar la vinculación directa con la vista
+        binding.btnViewBindingInflateFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(binding.container.getId(),new ViewBindingInflateFragment()).commitAllowingStateLoss();
+            }
+        });
     }
 }
