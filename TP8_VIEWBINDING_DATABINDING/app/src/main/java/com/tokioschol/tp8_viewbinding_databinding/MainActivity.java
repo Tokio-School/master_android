f37bd10c9e7e331ebcb941d246b527688519620d
manelcc <manelcc72@gmail.com>
2021-08-31 09:59:03 +0200

    TP8 VIEWBINDING - DATABINDING: add project

---
+++ TP8_VIEWBINDING_DATABINDING/app/src/main/java/com/tokioschol/tp8_viewbinding_databinding/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tokioschol.tp8_viewbinding_databinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
