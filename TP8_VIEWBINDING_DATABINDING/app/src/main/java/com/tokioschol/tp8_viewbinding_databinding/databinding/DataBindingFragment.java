6f8d890ff5d872b44ede9d1b080bf6b7590d1391
manelcc <manelcc72@gmail.com>
2021-09-18 12:24:30 +0200

    databinding bindadapter

--- TP8_VIEWBINDING_DATABINDING/app/src/main/java/com/tokioschol/tp8_viewbinding_databinding/databinding/DataBindingFragment.java
+++ TP8_VIEWBINDING_DATABINDING/app/src/main/java/com/tokioschol/tp8_viewbinding_databinding/databinding/DataBindingFragment.java
@@ -1,14 +1,22 @@
 package com.tokioschol.tp8_viewbinding_databinding.databinding;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.ColorRes;
+import androidx.annotation.DrawableRes;
+import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.BindingAdapter;
 import androidx.fragment.app.Fragment;
 
+import com.tokioschol.tp8_viewbinding_databinding.R;
 import com.tokioschol.tp8_viewbinding_databinding.databinding.domain.CivilStatus;
 import com.tokioschol.tp8_viewbinding_databinding.databinding.domain.Gender;
 import com.tokioschol.tp8_viewbinding_databinding.databinding.domain.UserModel;
@@ -28,11 +36,18 @@ public class DataBindingFragment extends Fragment {
     }
 
 
+    @BindingAdapter(value = {"app:colorView","app:srcVectorDrawable"},requireAll = true)
+    public static void setSrcVector(AppCompatImageView view, @ColorRes int color, Drawable resource){
+        if(view!=null){
+            view.setImageDrawable(resource);
+            view.setColorFilter(color);
+        }
+    }
 
     private UserModel provideUser() {
         UserModel userModel = new UserModel();
         userModel.setAge(25);
-        userModel.setName("manel");
+        //userModel.setName("manel");
         userModel.setLastName("cabezas");
         userModel.setGender(Gender.MALE);
         userModel.setCivilState(CivilStatus.MARRIED);
