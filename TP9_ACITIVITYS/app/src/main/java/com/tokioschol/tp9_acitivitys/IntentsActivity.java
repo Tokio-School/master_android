6268d5cdcac264799323d47b8950a57bc717d686
manelcc <manelcc72@gmail.com>
2021-09-19 20:42:53 +0200

    intents genericos

--- TP9_ACITIVITYS/app/src/main/java/com/tokioschol/tp9_acitivitys/IntentsActivity.java
+++ TP9_ACITIVITYS/app/src/main/java/com/tokioschol/tp9_acitivitys/IntentsActivity.java
@@ -1,4 +1,65 @@
 package com.tokioschol.tp9_acitivitys;
 
-public class IntentsActivity {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.provider.Settings;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tokioschol.tp9_acitivitys.databinding.ActivityIntentsBinding;
+
+/**
+ * Para ver más intents genericos ir a
+ *
+ * @see </https://developer.android.com/guide/components/intents-common?hl=es#java>
+ */
+public class IntentsActivity extends AppCompatActivity {
+    private ActivityIntentsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityIntentsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        super.onCreate(savedInstanceState);
+        binding.intentCrearAlarma.setOnClickListener(v ->
+                openWifiSettings()
+                //createAlarm("Poner alarma", 23, 45)
+        );
+        binding.intentCrearEnviarEmail.setOnClickListener(view -> composeEmail(new String[]{"manelcc72@gmail.com"},"sujeto"));
+
+
+    }
+
+    public void openWifiSettings() {
+        Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
+
+    public void createAlarm(String message, int hour, int minutes) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void composeEmail(String[] addresses, String subject) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 }
--- TP9_ACITIVITYS/app/src/main/java/com/tokioschol/tp9_acitivitys/MainActivity.java
+++ TP9_ACITIVITYS/app/src/main/java/com/tokioschol/tp9_acitivitys/MainActivity.java
@@ -2,13 +2,22 @@ package com.tokioschol.tp9_acitivitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.tokioschol.tp9_acitivitys.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.intentsBtn.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, IntentsActivity.class)));
+
     }
 }
