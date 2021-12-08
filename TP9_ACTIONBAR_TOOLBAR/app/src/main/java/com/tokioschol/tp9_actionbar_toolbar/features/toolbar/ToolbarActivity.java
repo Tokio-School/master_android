9d92172a265ec4c5f943a7392cea631436760a25
manelcc <manelcc72@gmail.com>
2021-12-08 18:26:35 +0100

    crear carpetas con los nuevos recursos para las actividades

--- TP9_ACTIONBAR_TOOLBAR/app/src/main/java/com/tokioschol/tp9_actionbar_toolbar/features/toolbar/ToolbarActivity.java
+++ TP9_ACTIONBAR_TOOLBAR/app/src/main/java/com/tokioschol/tp9_actionbar_toolbar/features/toolbar/ToolbarActivity.java
@@ -1,8 +1,13 @@
 package com.tokioschol.tp9_actionbar_toolbar.features.toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.tokioschol.tp9_actionbar_toolbar.R;
@@ -20,16 +25,25 @@ public class ToolbarActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-
-
-        binding.toolbar.setOnMenuItemClickListener(item -> {
-            if (item.getItemId() == R.id.shoppingcart) {
-                Toast.makeText(ToolbarActivity.this, "icono en toolbar pulsado", Toast.LENGTH_SHORT).show();
-                return true;
-            }
-            return false;
-        });
-
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.shoppingcart) {
+            Toast.makeText(ToolbarActivity.this, "icono en toolbar pulsado", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        if (item.getItemId() == R.id.shoppingcart2) {
+            Toast.makeText(ToolbarActivity.this, "icono en toolbar pulsado", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
