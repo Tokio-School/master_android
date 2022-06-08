8ce58256ab1c597997c9b67a12b1a00771e628ff
manelcc <manelcc72@gmail.com>
2022-06-08 20:45:41 +0200

    add new project codelab kotlin

--- MiPrimeraappkotlin/app/src/main/java/com/tokioschol/miprimeraappkotlin/CicloMotor.kt
+++
@@ -1,6 +0,0 @@
-package com.tokioschol.miprimeraappkotlin
-
-class CicloMotor: Moto() {
-
-
-}
--- MiPrimeraappkotlin/app/src/main/java/com/tokioschol/miprimeraappkotlin/genericFunc.kt
+++
@@ -1,9 +0,0 @@
-package com.tokioschol.miprimeraappkotlin
-
-infix fun String.agregar(value:String):String = this.plus(value)
-
-val sum: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
-
-fun executeIfTrue(condition:Boolean,block:()->Unit){
-    if (condition!=null && condition==true) block else print("don't execute func")
-}
