879512c09811d8698930df250b6c56bcc5d9c3a1
manelcc <manelcc72@gmail.com>
2022-06-16 20:04:07 +0200

    init project

---
+++ KOTLIN_MVVM_APP/app/src/main/java/com/tokioschol/travellingkotlin/presentation/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.tokioschol.travellingkotlin.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tokioschol.travellingkotlin.databinding.ActivityMainBinding
+import com.tokioschol.travellingkotlin.presentation.core.extension.viewBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
