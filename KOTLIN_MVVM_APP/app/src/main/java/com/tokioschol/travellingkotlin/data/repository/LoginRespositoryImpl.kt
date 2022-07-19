a5c320689f3815eb04522f9a400958714f7412c5
manelcc <manelcc72@gmail.com>
2022-07-19 20:55:43 +0200

    repository login

--- KOTLIN_MVVM_APP/app/src/main/java/com/tokioschol/travellingkotlin/data/repository/LoginRespositoryImpl.kt
+++ KOTLIN_MVVM_APP/app/src/main/java/com/tokioschol/travellingkotlin/data/repository/LoginRespositoryImpl.kt
@@ -1,25 +1,45 @@
 package com.tokioschol.travellingkotlin.data.repository
 
+import android.content.Context
+import android.util.Log
+import com.facebook.AccessToken
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
 import com.tokioschol.travellingkotlin.data.datasource.LoginDatasource
 import com.tokioschol.travellingkotlin.data.mappers.mapToUser
 import com.tokioschol.travellingkotlin.domain.models.User
 import com.tokioschol.travellingkotlin.domain.repository.LoginRespository
-import kotlinx.coroutines.delay
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class LoginRespositoryImpl @Inject constructor(
-    private val loginDatasource: LoginDatasource
-): LoginRespository {
+    private val loginDatasource: LoginDatasource,
+    @ApplicationContext private val context: Context,
+    private val auth: FirebaseAuth,
+) : LoginRespository {
 
-    override fun logIn(userName: String, password: String): Flow<User>  =
-        loginDatasource.logIng(userName,password).map { it.mapToUser() }
+    override fun logIn(userName: String, password: String): Flow<User> =
+        loginDatasource.logIng(userName, password).map { it.mapToUser() }
 
 
-    override suspend fun logInFaceBook(token: String): User {
-        delay(5000)
-        //Comprobamos el token y devolvemos usuario
-        return User("Jose","Apellido",23)
+    override fun logInFaceBook(token: AccessToken): Flow<User> = flow {
+        kotlin.run {
+            val credential = FacebookAuthProvider.getCredential("123")
+            auth.signInWithCredential(credential).await()?.let { authResult ->
+                Log.e("manel","DISPLAYNAME ${authResult.user?.displayName?:"nulo"}")
+                Log.e("manel","PHONE-NUMBER ${authResult.user?.phoneNumber?:"nulo"}")
+                Log.e("manel","FOTO ${authResult.user?.photoUrl?:"nulo"}")
+                Log.e("manel","ADDITIONAL-USER-INFO ${authResult.additionalUserInfo?.profile}")
+
+                emit(User(
+                    name = (authResult.additionalUserInfo?.profile?.get("first_name")?:"") as String,
+                    surName = (authResult.additionalUserInfo?.profile?.get("last_name")?:"") as String,
+                    age = -1))
+            }
+        }
     }
 }
