bd4b08c7a5b3fdbea4c1ccbf2eaf7315244dbe15
Victor Vergel <victorvergel@verinsis.com>
2023-10-03 17:21:02 +0200

    Clase 03

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Clase03_Ejem01_ColocandoComponentesYUsandolos"
+include(":app")
+ 
