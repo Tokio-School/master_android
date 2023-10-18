97b7af2edd188067f7e7dc56a1544d327b5e9ec2
Victor Vergel <victorvergel@verinsis.com>
2023-10-18 17:19:39 +0200

    Antes de la clase 06

---
+++ Clases2023-24/Clase06_Ejem02_RecuperandoRG_CB_LV/app/src/main/java/com/example/clase06_ejem02_recuperandorg_cb_lv/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.clase06_ejem02_recuperandorg_cb_lv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ Clases2023-24/Clase06_Ejem02_RecuperandoRG_CB_LV/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Clase06_Ejem02_RecuperandoRG_CB_LV"
+include(":app")
+ 
