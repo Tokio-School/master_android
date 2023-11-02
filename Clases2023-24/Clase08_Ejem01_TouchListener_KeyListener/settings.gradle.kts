2ac334860508fbb0bf1778e01545e540501db373
Victor Vergel <victorvergel@verinsis.com>
2023-11-02 19:30:23 +0100

    Antes de la clase 08

---
+++ Clases2023-24/Clase08_Ejem01_TouchListener_KeyListener/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Clase08_Ejem01_TouchListener_KeyListener"
+include(":app")
+ 
---
+++ Clases2023-24/Clase08_Practicando/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lista;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        lista=((ListView)findViewById(R.id.lv_datos));
+        lista.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, getResources().getStringArray(R.array.planets_array)));
+
+        ((Button)findViewById(R.id.b_recuperarDatos)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //El siguiente método sólo funciona cuando el adapatador es del tipo CursorAdapter o SimpleCursorAdapter
+                Log.d("depurando", Arrays.toString(lista.getCheckedItemIds())+"-");
+
+
+                //Así no hay necesidad de coger otro adaptador no utilizado hasta el momento como el CursorAdapter.
+                SparseBooleanArray checkedItemPositions = lista.getCheckedItemPositions();
+                int itemCount = lista.getCount();
+                for (int i = 0; i < itemCount; i++) {
+                    if (checkedItemPositions.get(i)) {
+                        // El elemento en la posición 'i' está seleccionado
+                        // Haz algo con este elemento
+                        String selectedItem = (String) lista.getItemAtPosition(i);
+                        Log.d("depurando", selectedItem);
+                    }
+                }
+
+            }
+        });
+    }
+}
