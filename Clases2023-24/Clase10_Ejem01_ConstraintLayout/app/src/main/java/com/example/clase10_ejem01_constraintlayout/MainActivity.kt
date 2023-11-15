cb4998a930f940e469c94000dcf0afebcc598540
Victor Vergel <victorvergel@verinsis.com>
2023-11-15 17:28:33 +0100

    Antes de la clase ContrastraintLayout. Clase 10.

---
+++ Clases2023-24/Clase10_Ejem01_ConstraintLayout/app/src/main/java/com/example/clase10_ejem01_constraintlayout/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.clase10_ejem01_constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+    }
+}
