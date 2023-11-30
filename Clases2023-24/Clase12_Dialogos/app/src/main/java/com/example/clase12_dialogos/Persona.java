5a9fc2d443c949cf32fa04cb4c59160c4f61f49b
Victor Vergel <victorvergel@verinsis.com>
2023-11-30 07:53:21 +0100

    Después de la clase de FrameLayout y Diálogos de confirmación. Clase 12

---
+++ Clases2023-24/Clase12_Dialogos/app/src/main/java/com/example/clase12_dialogos/Persona.java
@@ -0,0 +1,37 @@
+package com.example.clase12_dialogos;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+
+    public Persona() {
+    }
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
