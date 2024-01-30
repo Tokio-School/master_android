ae421eb702ee2e41704e45bd1673db21be6edc5d
Victor Vergel <victorvergel@verinsis.com>
2024-01-30 16:55:21 +0100

    Actualización de la clase 13 sobre Databinding por estar mal actualizada en github

--- Clases2023-24/Clase13_DataBindingFinal_/app/build.gradle.kts
+++ Clases2023-24/Clase13_DataBindingFinal_/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.clase13_databinding"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.clase13_databinding"
@@ -29,12 +29,26 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
 }
 
 dependencies {
 
+    constraints {
+        implementation("org.jetbrains.kotlin-stdlib-jdk7:1.8.0") {
+            because("kotlin-stdlib-jdk7 is now apart of kotlin-stdlib-jdk8")
+        }
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.0") {
+            because("kotlin-stdlib-jdk8 is now apart of kotlin-stdlib-jdk8")
+        }
+    }
+
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
